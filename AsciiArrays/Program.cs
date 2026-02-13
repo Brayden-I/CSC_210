c1fbb842f6577be194ba900c224e0360b8ad605f
Brayden Imada <bi1012440@gmail.com>
2026-02-13 09:59:24 -0600

    Finished hash indexing loop

---
+++ AsciiArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AsciiArrays;
+
+class Program
+{
+    public static int HashFunction(string String, int ArrayLength) // Gets the sum of ascii from a string
+    {
+        int sum = 0;
+        char[] charArray = String.ToCharArray(); // Get the char array from string
+
+        foreach (char c in charArray)
+        {
+            sum += Convert.ToInt32(c); // Converting to Int returns the char's ASCII
+        }
+
+        return sum % ArrayLength; // Return result
+    }
+
+    static void Main(string[] args)
+    {
+        // Create our key value pair array
+        string[][] MultiDimArray = new string[][]
+        {
+            // An array
+            ["apple", "red"], // with arrays inside
+            ["banana", "yellow"],
+            ["plum", "purple"]
+        };
+
+        foreach (string[] subArray in MultiDimArray) // Loop through each array in the multi-dimensional array
+        {
+            string key = subArray[0]; // The key is the first element
+            int hashIndex = HashFunction(key, MultiDimArray.Length);
+            Console.WriteLine($"Key: '{key}' -> Hash Index: {hashIndex}");
+        }
+    }
+}
